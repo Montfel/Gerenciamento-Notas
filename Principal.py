6471b2a88f7bf2b8ae27f6224ad02873bbd6b9f9
Luís Felipe Monteiro <felipemonteirose@gmail.com>
2020-11-20 23:51:56 -0300

    Menu feito

--- Principal.py
+++ Principal.py
@@ -1,1 +1,33 @@
-print('TO on!')
+from Classe import Professor, Aluno, Disciplina, Nota
+
+while True:
+    print('\nMenu da Escola\n\n'
+          '1 - Cadastro de Professores\n'
+          '2 - Cadastro de Alunos\n'
+          '3 - Cadastro de Disciplinas\n'
+          '4 - Cadastro de Notas\n'
+          '5 - Relatório de Notas\n'
+          '0 - Sair\n')
+
+    escolha = input('Escolha uma das opções acima: ')
+
+    if escolha == '0':
+        break
+    
+    elif escolha == '1':
+        pass
+
+    elif escolha == '2':
+        pass
+
+    elif escolha == '3':
+        pass
+
+    elif escolha == '4':
+        pass
+
+    elif escolha == '5':
+        pass
+
+    else:
+        print('\nOpção invalida! Digite novamente.')
