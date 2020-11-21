3f17cf335057ab43d899a91f0d2155db99b5ad13
GabrielSouzaSE <gabrielsouzase@gmail.com>
2020-11-21 00:01:19 -0300

    Update Classe.py

--- Classe.py
+++ Classe.py
@@ -1,41 +1,41 @@
 from datetime import date
 class Professor:
-    Nome = ''
-    Matricula = ''
-    Data_nascimento = date
+    nome = ''
+    matricula = ''
+    data_nascimento = date
 
     def __init__(self, nome, matricula, data_nascimento):
-        self.Nome = nome
-        self.Matricula = matricula
-        self.Data_nascimento = data_nascimento
+        self.nome = nome
+        self.matricula = matricula
+        self.data_nascimento = data_nascimento
 
 class Aluno:
-    Nome = ''
-    Matricula = ''
-    Data_nascimento = date
+    nome = ''
+    matricula = ''
+    data_nascimento = date
 
     def __init__(self, nome, matricula, data_nascimento):
-        self.Nome = nome
-        self.Matricula = matricula
-        self.Data_nascimento = data_nascimento
+        self.nome = nome
+        self.matricula = matricula
+        self.data_nascimento = data_nascimento
 
 class Disciplina:
-    Codigo = ''
-    Nome = ''
-    Matricula_professor = ''
+    codigo = ''
+    nome = ''
+    matricula_professor = ''
     
     def __init__(self, codigo, nome, matricula_professor):
-        self.Codigo = codigo
-        self.Nome = nome
-        self.Matricula_professor = matricula_professor
+        self.codigo = codigo
+        self.nome = nome
+        self.matricula_professor = matricula_professor
 
 class Nota:
-    Codigo_disciplina = ''
-    Matricula_aluno = ''
-    Nota1 = 0.0
-    Nota2 = 0.0
+    codigo_disciplina = ''
+    matricula_aluno = ''
+    nota1 = 0.0
+    nota2 = 0.0
     
     def __init__(self, codigo, nome, matricula_professor):
-        self.Codigo = codigo
-        self.Nome = nome
-        self.Matricula_professor = matricula_professor
+        self.codigo = codigo
+        self.nome = nome
+        self.matricula_professor = matricula_professor
